7a00662e852fd2982cab55c4501832859dcc711e
Whisper-Jhin <2962402755@qq.com>
2020-03-16 23:48:43 +0800

    So hard!

--- day10/Exercise10.12.py
+++ day10/Exercise10.12.py
@@ -1,3 +1,26 @@
+def in_bisect(word_list, word):
+    if len(word_list) == 0:
+        return False
+
+    i = len(word_list) // 2
+    if word_list[i] == word:
+        return True
+
+    if word_list[i] > word:
+        # search the first half
+        return in_bisect(word_list[:i], word)
+    else:
+        # search the second half
+        return in_bisect(word_list[i + 1:], word)
+
+
+def make_word_list():
+    word_list = []
+    fin = open('words.txt')
+    for line in fin:
+        word = line.strip()
+        word_list.append(word)
+    return word_list
 def interlock(word_list, word):
     """Checks whether a word contains two interleaved words.
 
